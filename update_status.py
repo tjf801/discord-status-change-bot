2d59b09e6a9b98066932f3e2a89bee1e9381f4a5
tjf801 <fuscofamily0@gmail.com>
2021-09-28 09:13:42 -0400

    improve code readability

--- update_status.py
+++ update_status.py
@@ -37,76 +37,78 @@ async def on_ready():
 async def parse_command(raw_command_text: str):
 	assert raw_command_text[0] == '!'
 	
-	command, args, text = re.match("!([a-zA-Z]+)?(\{.*\})? ?(.*)\n", raw_command_text).groups()
+	COMMAND_REGEX = r"!([a-zA-Z]+)?(\{.*\})? ?(.*)\n"
+	
+	command, args, text = re.match(COMMAND_REGEX, raw_command_text).groups()
 	
 	default_args = {"name": text, "start": datetime.datetime.now()}
 	command_args = {**default_args, **(eval(args) if args is not None else {})}
 	
-	activity = None
-	
-	if command=="playing":
+	if command == "playing":
 		activity = discord.Game(**command_args)
 		log(f"set status to Playing '{text}'")
-	
-	elif command=="streaming":
+	elif command == "streaming":
 		activity = discord.Streaming(**command_args)
 		log(f"set status to Streaming '{text}' at {command_args['url']}")
-	
-	elif command=="listening":
+	elif command == "listening":
 		activity = discord.Activity(type=discord.ActivityType.listening, **command_args)
 		log(f"set status to Listening to '{text}'")
-	
-	elif command=="watching":
+	elif command == "watching":
 		activity = discord.Activity(type=discord.ActivityType.watching, **command_args)
 		log(f"set status to Watching '{text}'")
-	
-	elif command=="competing":
+	elif command == "competing":
 		activity = discord.Activity(type=discord.ActivityType.competing, **command_args)
 		log(f"set status to Competing in '{text}'")
-	
-	elif command=='py':
+	elif command == 'py':
 		custom_args = {"state": eval(text), **command_args, "name": "not used"}
 		activity = discord.Activity(type=discord.ActivityType.custom, **custom_args)
 		log(f"set status to '{custom_args['state'][:128]}'")
-	
 	elif command is None:
 		custom_args = {"state": default_args['name'], **command_args, "name": "not used"}
 		activity = discord.Activity(type=discord.ActivityType.custom, **custom_args)
 		log(f"set status to '{custom_args['state'][:128]}'")
-	
-	else: raise NameError(f"\033[31munknown command \033[36m{command}\033[00m")
+	else:
+		raise NameError(f"\033[31munknown command \033[36m{command}\033[00m")
 	
 	await client.change_presence(activity=activity, afk=True)
 
 @tasks.loop(seconds=TIME_DIFF.seconds)
 async def update_status(status: str = None, is_regex: bool = False):
 	with open("./discord_statuses.txt", "r+", encoding='utf8') as f:
-		if status is None: line: str = random.choice(f.readlines())
+		if status is None:
+			line: str = random.choice(f.readlines())
 		else:
 			if not is_regex:
 				lines = list(filter(lambda x: status.lower() in x.lower(), f.readlines()))
 			else:
 				try: regex = re.compile(status)
-				except re.error: return print(f"\033[31minvalid regex expression '\033[36m{status}\033[31m'\033[00m")
+				except re.error:
+					print(f"\033[31minvalid regex expression '\033[36m{status}\033[31m'\033[00m")
+					return
 				lines = list(filter(lambda x: regex.search(x) is not None, f.readlines()))
-			if len(lines) == 0: return print(f"\033[31mstatus with '\033[36m{status}\033[31m' not found\033[00m")
+			if len(lines) == 0:
+				print(f"\033[31mstatus with '\033[36m{status}\033[31m' not found\033[00m")
+				return
 			line: str = random.choice(list(lines))
 		
 		if line[0] == '!': await parse_command(line)
 		else:
-			#NOTE: why the FUCK does discord.py need `name` to be a non-empty string specifically
-			#AGHHHHHHHHH i hate this api
-			await client.change_presence(activity=discord.Activity(type=discord.ActivityType.custom, state=line[:-1][:128], name="this is not used"), afk=True)
+			# NOTE: why the FUCK does discord.py need `name` to be a non-empty string specifically
+			# AGHHHHHHHHH i hate this api
+			activity = discord.Activity(type=discord.ActivityType.custom, state=line[:-1][:128], name="this is not used")
+			await client.change_presence(activity=activity, afk=True)
 			log(f"set status to '{line[:-1][:128]}'")
 
 @update_status.before_loop
 async def delay_status_update():
+	from datetime import datetime, timedelta
+	
 	# waits until the next multiple of time_difference
-	def get_time_to_wait_until(time_difference: datetime.timedelta) -> datetime.datetime:
-		now: datetime.datetime = datetime.datetime.now()
-		this_morning: datetime.datetime = now.replace(hour=0,minute=0,second=0,microsecond=0)
+	def get_time_to_wait_until(time_difference: timedelta) -> datetime:
+		now: datetime = datetime.datetime.now()
+		day_start: datetime = now.replace(hour=0, minute=0, second=0, microsecond=0)
 		
-		i: datetime.datetime = this_morning + OFFSET
+		i: datetime.datetime = day_start + OFFSET
 		while i < now:
 			i += time_difference
 		
